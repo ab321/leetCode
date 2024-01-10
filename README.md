ddcac0b14a293c949f064889cd689af45b584f45
ab321 <abdubed2@gmail.com>
2024-01-10 12:56:30 +0100

    feat: finished 2 versions of palindromeNumber and updated readme

--- README.md
+++ README.md
@@ -11,6 +11,8 @@
 
 ## Runtime & Memory of Solutions
 
-| # | Title | Solution                  | Runtime | Memory |
-|---| ----- |---------------------------|--------| ------ |
-|1|[Two Sum](https://leetcode.com/problems/two-sum/)| [C#](./twoSum/Program.cs) | 135 ms	| 47.72 MB |
+| # | Title                                                                    | Solution                            | Runtime | Memory   |
+|---|--------------------------------------------------------------------------|-------------------------------------|---------|----------|
+| 1 | [Two Sum](https://leetcode.com/problems/two-sum/)                        | [C#](./twoSum/Program.cs)           | 135 ms	 | 47.72 MB |
+| 2 | [Palindrome Number V1](https://leetcode.com/problems/palindrome-number/) | [C#](./palindromeNumber/Program.cs) | 61 ms	  | 33.31 MB |
+| 3 | [Palindrome Number V2](https://leetcode.com/problems/palindrome-number/) | [C#](./palindromeNumber/Program.cs) | 49 ms	  | 51.24 MB |
---
+++ palindromeNumber/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+   Given an integer x, return true if x is a palindrome, and false otherwise.
+
+   Example 1:
+
+   Input: x = 121
+   Output: true
+*/
+
+
+
+//Version 1
+
+/*
+bool IsPalindrom(int x)
+{
+   if (x >= 0)
+   {
+      int maxLength = x.ToString().Length - 1;
+      string input = x.ToString();
+
+      for (int i = 0; i < maxLength+1; i++)
+      {
+         if (input[i] != input[maxLength - i])
+            return false;
+      }
+
+      return true;
+   }
+
+   return false;
+}
+*/
+
+
+//Version 2
+
+bool IsPalindrom(int x)
+{
+   if (x >= 0)
+   {
+      int[] numbArr = new int[x.ToString().Length];
+      int[] numbArr2 = new int[x.ToString().Length];
+
+      for (int i = 0; i < x.ToString().Length; i++)
+      {
+         numbArr[i] = x.ToString()[i] - 48;
+      }
+
+      int counter = 0;
+
+      for (int i = x.ToString().Length - 1; i >= 0; i--)
+      {
+         numbArr2[counter] =  (int) x.ToString()[i] - 48;
+         counter++;
+      }
+
+      for (int i = 0; i < numbArr.Length; i++)
+      {
+         if (numbArr[i] != numbArr2[i])
+         {
+            return false;
+         }
+      }
+
+      return true;
+   }
+   else
+   {
+      return false;
+   }
+}
