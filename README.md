c491612c8490f84ee15e1641a2cf1bb9e7785ea8
ab321 <abdubed2@gmail.com>
2024-01-10 14:10:22 +0100

    feat: finished 2 versions of romanToInteger and updated readme

--- README.md
+++ README.md
@@ -16,3 +16,6 @@
 | 1 | [Two Sum](https://leetcode.com/problems/two-sum/)                        | [C#](./twoSum/Program.cs)           | 135 ms	 | 47.72 MB |
 | 2 | [Palindrome Number V1](https://leetcode.com/problems/palindrome-number/) | [C#](./palindromeNumber/Program.cs) | 61 ms	  | 33.31 MB |
 | 3 | [Palindrome Number V2](https://leetcode.com/problems/palindrome-number/) | [C#](./palindromeNumber/Program.cs) | 49 ms	  | 51.24 MB |
+| 4 | [Roman to Integer V1](https://leetcode.com/problems/roman-to-integer/)   | [C#](./romanToInteger/Program.cs)   | 48 ms	  | 50.31 MB |
+| 5 | [Roman to Integer V2](https://leetcode.com/problems/roman-to-integer/)   | [C#](./romanToInteger/Program.cs)   | 69 ms	  | 51.25 MB |
+
---
+++ romanToInteger/Program.cs
@@ -0,0 +1,111 @@
+﻿/*
+
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+
+
+   Example 1:
+
+   Input: s = "III"
+   Output: 3
+   Explanation: III = 3.
+
+   Example 2:
+
+   Input: s = "LVIII"
+   Output: 58
+   Explanation: L = 50, V= 5, III = 3.
+
+   Example 3:
+
+   Input: s = "MCMXCIV"
+   Output: 1994
+   Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+      Symbol       Value
+   I             1
+   V             5
+   X             10
+   L             50
+   C             100
+   D             500
+   M             1000
+
+
+
+// Version 1
+
+int RomanToInt(string s) {
+   Dictionary<string, int> romanDigits = new Dictionary<string, int>()
+   {
+      {"I", 1 },
+      {"V", 5},
+      {"X", 10},
+      {"L", 50},
+      {"C", 100},
+      {"D", 500},
+      {"M", 1000}
+   };
+   
+   int result = 0;
+
+   var resultsArr = new int[s.Length];
+
+   for (int i = 0; i < s.Length; i++)
+   {
+      romanDigits.TryGetValue(s[i].ToString(), out resultsArr[i]);
+   }
+
+   for (int i = 0; i < resultsArr.Length; i++)
+   {
+      if (i < resultsArr.Length - 1 && resultsArr[i] < resultsArr[i + 1])
+      {
+         result += (resultsArr[i + 1] - resultsArr[i]);
+         i++;
+      }
+      else
+      {
+         result += resultsArr[i];
+      }
+   }
+
+   return result;
+}
+*/
+
+string input = "MCMXCIV";
+
+Console.WriteLine(RomanToInt(input));
+
+//Version 2
+
+int RomanToInt(string s) {
+
+   Dictionary<string, int> romanDigits = new Dictionary<string, int>()
+   {
+      {"I", 1 },
+      {"V", 5},
+      {"X", 10},
+      {"L", 50},
+      {"C", 100},
+      {"D", 500},
+      {"M", 1000}
+   };
+   
+   int result = 0;
+
+   for (int i = 0; i < s.Length; i++)
+   {
+      if (i < s.Length - 1 && romanDigits[s[i].ToString()] < romanDigits[s[i + 1].ToString()])
+      {
+         result -= romanDigits[s[i].ToString()];
+      }
+      else
+      {
+         result += romanDigits[s[i].ToString()];
+      }
+   }
+
+   return result;
+   
+}
