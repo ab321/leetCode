82ff5aeaa59fd449b0fcda85474f4254159d84ca
ab321 <abdubed2@gmail.com>
2024-01-12 20:38:48 +0100

    feat: finished Remove Duplicates from Sorted Array and updated readme

---
+++ removeDuplicatesfromSortedArray/Program.cs
@@ -0,0 +1,23 @@
+﻿
+var numbers = new int[]
+{
+    0,0,1,1,1,2,2,3,3,4
+};
+
+RemoveDuplicates(numbers);
+
+int RemoveDuplicates(int[] nums)
+{
+    int k = 1;
+
+    for (int i = 1; i < nums.Length; i++)
+    {
+        if (nums[i] != nums[i - 1])
+        {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
