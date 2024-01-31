fcd60d468b80e68c33f6381c41c31252e062efc6
ab321 <abdubed2@gmail.com>
2024-01-31 18:27:57 +0100

    feat: finished Majority Element Problem leetCode with dotNet

---
+++ dotNet/MajorityElement/MajorityElement/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Hello, World!");
+
+var nums = new int[] { 6,5,5};
+
+Console.WriteLine(majorityElement(nums));
+
+int majorityElement(int[] nums)
+{
+    var numbersDic = new Dictionary<int, int>();
+
+    foreach (var number in nums)
+    {
+        if (!numbersDic.TryAdd(number, 1))
+        { 
+            numbersDic.TryGetValue(number, out var currentCount);
+            numbersDic[number] =  currentCount + 1;
+        }
+    }
+    
+    return numbersDic.MaxBy(x => x.Value).Key;
+}
